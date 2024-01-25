6ad67c5e9d8dc0dc89103c64c44121d2501d1468
masamune <emopanda1337@gmail.com>
2024-01-25 16:24:33 +0700

    ContactMediumUsage renamed to ContactMediumUsageType

--- src/main/java/org/kata/dto/ContactMediumDto.java
+++ src/main/java/org/kata/dto/ContactMediumDto.java
@@ -6,7 +6,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.extern.jackson.Jacksonized;
 import org.kata.dto.enums.ContactMediumType;
-import org.kata.dto.enums.ContactMediumUsage;
+import org.kata.dto.enums.ContactMediumUsageType;
 
 
 @Data
@@ -23,7 +23,7 @@ public class ContactMediumDto {
     private ContactMediumType type;
 
     @Schema(description = "Contact Medium Usage Type", example = "BUSINESS")
-    private ContactMediumUsage usage;
+    private ContactMediumUsageType usage;
 
     @Schema(description = "Value", example = "example@gmail.com")
     private String value;
--- src/main/java/org/kata/dto/enums/ContactMediumUsageType.java
+++ src/main/java/org/kata/dto/enums/ContactMediumUsageType.java
@@ -1,6 +1,6 @@
 package org.kata.dto.enums;
 
-public enum ContactMediumUsage {
+public enum ContactMediumUsageType {
     PERSONAL,
 
     BUSINESS
